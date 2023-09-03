7d04ed97d2a855417648f39c2464a557fa5ea2b3
Vitaly <ivanov.arduan@yandex.ru>
2023-09-03 17:42:24 +0300

    Second commit.

--- women/urls.py
+++ women/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from .views import index, categories
+from .views import index, categories, categories_by_slug
 
 urlpatterns = [
     path('', index),
-    path('cat/', categories),
+    path('cat/<int:cat_id>/', categories),
+    path('cat/<slug:cat_slug>/', categories_by_slug),
 ]
