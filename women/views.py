c6ed80619898229655573999fb0d32cbeb8e8908
Vitaly <ivanov.arduan@yandex.ru>
2023-09-07 10:47:33 +0300

    Fix files html

--- women/views.py
+++ women/views.py
@@ -2,14 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template.loader import render_to_string
 
+menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
 
 def index(request):
-    data = {'title': 'Главная страница'}
-    return render(request, 'index.html', data)
+    data = {'title': 'Главная страница',
+            'menu': menu,
+            'float': 34.53,
+            'lst': [2, 4, 3, 'Hello World', True],
+            'set': {3, 4, 5, 6},
+            'dict': {'name': 'Vitaliy', 'age': 63}
+            }
+    return render(request, 'index.html', context=data)
 
 
 def about(request):
-    return render(request, 'about.html', {'title': 'О сайте'})
+    return render(request, 'about.html', {'title': 'О сайте',})
 
 
 def categories(request, cat_id):
