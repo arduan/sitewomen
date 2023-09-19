4565af8e4ce02a0689f37287292588119e8f8466
Vitaly <ivanov.arduan@yandex.ru>
2023-09-19 15:39:49 +0300

    Slug...

--- women/urls.py
+++ women/urls.py
@@ -1,7 +1,9 @@
-
-from django.urls import path
+from django.urls import path, re_path, register_converter
 from . import views
+from . import converters
 
+
+register_converter(converters.FourDigitYearConverter, "year4")
 
 urlpatterns = [
     path('', views.index, name='home'),  # http://127.0.0.1:8000
@@ -9,6 +11,6 @@ urlpatterns = [
     path('addpage/', views.addpage, name='add_page'),
     path('contact/', views.contact, name='contact'),
     path('login/', views.login, name='login'),
-    path('post/<int:post_id>/', views.show_post, name='post'),
+    path('post/<slug:post_slug>/', views.show_post, name='post'),
     path('category/<int:cat_id>/', views.show_category, name='category'),
 ]
--- women/views.py
+++ women/views.py
@@ -1,22 +1,25 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect, HttpResponsePermanentRedirect
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.template.loader import render_to_string
 from django.template.defaultfilters import slugify
+
+from .models import Women
 
 menu = [{'title': "О сайте", 'url_name': 'about'},
         {'title': "Добавить статью", 'url_name': 'add_page'},
         {'title': "Обратная связь", 'url_name': 'contact'},
         {'title': "Войти", 'url_name': 'login'}
-        ]
+]
 
 data_db = [
-    {'id': 1, 'title': 'Анджелина Джоли', 'content': ''' (англ. Angelina Jolie, при рождении Войт (англ. Voight), ранее Джоли Питт (англ. Jolie Pitt); род. 4 июня 1975, Лос-Анджелес, Калифорния, США) — американская актриса кино, телевидения и озвучивания, кинорежиссёр, сценаристка, продюсер, фотомодель, посол доброй воли ООН.
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': '''<h1>Анджелина Джоли</h1> (англ. Angelina Jolie[7], при рождении Войт (англ. Voight), ранее Джоли Питт (англ. Jolie Pitt); род. 4 июня 1975, Лос-Анджелес, Калифорния, США) — американская актриса кино, телевидения и озвучивания, кинорежиссёр, сценаристка, продюсер, фотомодель, посол доброй воли ООН.
     Обладательница премии «Оскар», трёх премий «Золотой глобус» (первая актриса в истории, три года подряд выигравшая премию) и двух «Премий Гильдии киноактёров США».''',
      'is_published': True},
-    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
     {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
 ]
+
 cats_db = [
     {'id': 1, 'name': 'Актрисы'},
     {'id': 2, 'name': 'Певицы'},
@@ -25,10 +28,12 @@ cats_db = [
 
 
 def index(request):
+    posts = Women.objects.filter(is_published=1)
+
     data = {
         'title': 'Главная страница',
         'menu': menu,
-        'posts': data_db,
+        'posts': posts,
         'cat_selected': 0,
     }
     return render(request, 'women/index.html', context=data)
@@ -38,8 +43,17 @@ def about(request):
     return render(request, 'women/about.html', {'title': 'О сайте', 'menu': menu})
 
 
-def show_post(request, post_id):
-    return HttpResponse(f"Отображение статьи с id = {post_id}")
+def show_post(request, post_slug):
+    post = get_object_or_404(Women, slug=post_slug)
+
+    data = {
+        'title': post.title,
+        'menu': menu,
+        'post': post,
+        'cat_selected': 1,
+    }
+
+    return render(request, 'women/post.html', data)
 
 
 def addpage(request):
@@ -56,7 +70,7 @@ def login(request):
 
 def show_category(request, cat_id):
     data = {
-        'title': 'Отображение по рубрикам',
+        'title': 'Отображение по рубоикам',
         'menu': menu,
         'posts': data_db,
         'cat_selected': cat_id,
